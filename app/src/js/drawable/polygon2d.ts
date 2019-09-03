d7e76c23c05940622844c6335e57b5a48274170b
AimAlex <alex-li@sjtu.edu.cn>
2019-09-03 13:44:38 -0700

    to merge master

--- app/src/js/drawable/polygon2d.ts
+++ app/src/js/drawable/polygon2d.ts
@@ -10,19 +10,18 @@ import { Vector2D } from '../math/vector2d'
 import { Edge2D, makeEdge2DStyle } from './edge2d'
 import { DrawMode, Label2D } from './label2d'
 import { makePoint2DStyle, Point2D } from './point2d'
-// import { makePoly2DStyle, Poly2D } from './poly2d'
 import { blendColor, Context2D, encodeControlColor, getColorById } from './util'
 
 // type Shape = Poly2D | Point2D
 
-const _DEFAULT_VIEW_EDGE_STYLE = makeEdge2DStyle({ lineWidth: 4 })
-const _DEFAULT_VIEW_POINT_STYLE = makePoint2DStyle({ radius: 8 })
-const _DEFAULT_VIEW_HIGH_POINT_STYLE = makePoint2DStyle({ radius: 12 })
-const _DEFAULT_CONTROL_EDGE_STYLE = makeEdge2DStyle({ lineWidth: 10 })
-const _DEFAULT_CONTROL_POINT_STYLE = makePoint2DStyle({ radius: 12 })
+const DEFAULT_VIEW_EDGE_STYLE = makeEdge2DStyle({ lineWidth: 4 })
+const DEFAULT_VIEW_POINT_STYLE = makePoint2DStyle({ radius: 8 })
+const DEFAULT_VIEW_HIGH_POINT_STYLE = makePoint2DStyle({ radius: 12 })
+const DEFAULT_CONTROL_EDGE_STYLE = makeEdge2DStyle({ lineWidth: 10 })
+const DEFAULT_CONTROL_POINT_STYLE = makePoint2DStyle({ radius: 12 })
 
 /** list all states */
-export enum Polygon2DState {
+enum Polygon2DState {
   Free,
   Draw,
   Reshape,
@@ -31,24 +30,15 @@ export enum Polygon2DState {
 
 export class Polygon2D extends Label2D {
   // private _shapes: Shape[]
-  private _vertices: Point2D[]
-  private _edges: Edge2D[]
-  private _closed: boolean
-  private _state: Polygon2DState
+  private vertices: Point2D[]
+  private edges: Edge2D[]
+  // private _closed: boolean
+  private state: Polygon2DState
   constructor () {
     super()
-    // this._shapes = [
-    //   new Poly2D(),
-    //   // to be delete
-    //   new Point2D(), new Point2D(), new Point2D(), new Point2D(),
-    //   new Point2D(), new Point2D(), new Point2D(), new Point2D()
-    // ]
-
-    this._vertices = []
-    this._edges = []
-    this._closed = false
-    this._state = Polygon2DState.Free
-
+    this.vertices = []
+    this.edges = []
+    this.state = Polygon2DState.Free
   }
 
    /** replace with get vertex edge state*/
@@ -56,57 +46,67 @@ export class Polygon2D extends Label2D {
   //   return this._shapes
   // }
 
-  public draw (_context: Context2D, _ratio: number, _mode: DrawMode): void {
-    // const self = this
+  public draw (context: Context2D, ratio: number, mode: DrawMode): void {
+    const self = this
 
-    // let pointStyle = makePoint2DStyle()
-    // let highPointStyle = makePoint2DStyle()
-    // let edgeStyle = makeEdge2DStyle()
-    // let assignColor: (i: number) => number[] = () => [0]
+    let pointStyle = makePoint2DStyle()
+    let highPointStyle = makePoint2DStyle()
+    let edgeStyle = makeEdge2DStyle()
+    let assignColor: (i: number) => number[] = () => [0]
 
-    // switch (mode) {
-    //   case DrawMode.VIEW:
-    //     pointStyle = _.assign(pointStyle, DEFAULT_VIEW_POINT_STYLE)
-    //     highPointStyle = _.assign(highPointStyle, DEFAULT_VIEW_HIGH_POINT_STYLE)
-    //     edgeStyle = _.assign(edgeStyle, DEFAULT_VIEW_EDGE_STYLE)
+    switch (mode) {
+      case DrawMode.VIEW:
+        pointStyle = _.assign(pointStyle, DEFAULT_VIEW_POINT_STYLE)
+        highPointStyle = _.assign(highPointStyle, DEFAULT_VIEW_HIGH_POINT_STYLE)
+        edgeStyle = _.assign(edgeStyle, DEFAULT_VIEW_EDGE_STYLE)
 
-    //     assignColor = (i: number): number[] => {
-    //       if (i % 2 === 0 && i > 0) {
-    //         return blendColor(self._color, [255, 255, 255], 0.7)
-    //       }
-    //       else {
-    //         return self._color
-    //       }
-    //     }
-    //     break
-    //   case DrawMode.CONTROL:
-    //     pointStyle = _.assign(pointStyle, DEFAULT_CONTROL_POINT_STYLE)
-    //     highPointStyle = _.assign(highPointStyle, DEFAULT_CONTROL_POINT_STYLE)
-    //     edgeStyle = _.assign(edgeStyle, DEFAULT_CONTROL_EDGE_STYLE)
-    //     assignColor = (i: number): number[] => {
-    //       return encodeControlColor(self._index, i)
-    //     }
-    //     break
-    // }
+        assignColor = (i: number): number[] => {
+          if (i % 2 === 0 && i > 0) {
+            return blendColor(self._color, [255, 255, 255], 0.7)
+          } else {
+            return self._color
+          }
+        }
+        break
+      case DrawMode.CONTROL:
+        pointStyle = _.assign(pointStyle, DEFAULT_CONTROL_POINT_STYLE)
+        highPointStyle = _.assign(highPointStyle, DEFAULT_CONTROL_POINT_STYLE)
+        edgeStyle = _.assign(edgeStyle, DEFAULT_CONTROL_EDGE_STYLE)
+        assignColor = (i: number): number[] => {
+          return encodeControlColor(self._index, i)
+        }
+        break
+    }
+
+    edgeStyle.color = assignColor(0)
+    for (const edge of this.edges) {
+      edge.draw(context, ratio, edgeStyle)
+    }
 
     // const poly = self._shapes[0]
     // edgeStyle.color = assignColor(0)
     // poly.draw(context, ratio, polyStyle)
-    // /**control free  */
-    // if (mode === DrawMode.CONTROL || this._selected || this._highlighted) {
-    //   for (let i = 1; i <= 8; i += 1) {
-    //     let style
-    //     if (i === self._highlightedHandle) {
-    //       style = highPointStyle
-    //     }
-    //     else {
-    //       style = pointStyle
-    //     }
-    //     style.color = assignColor(i)
-    //     const point = self._shapes[i] as Point2D
-    //     point.draw(context, ratio, style)
-    //   }
-    // }
+    /**control free  */
+    if (mode === DrawMode.CONTROL || this._selected || this._highlighted) {
+      for (let i = 0; i < this.vertices.length; ++i) {
+        const style = highPointStyle
+        style.color = assignColor(i)
+        this.vertices[i].draw(context, ratio, style)
+      }
+
+      // for (let i = 1; i <= 8; i += 1) {
+      //   let style
+      //   if (i === self._highlightedHandle) {
+      //     style = highPointStyle
+      //   }
+      //   else {
+      //     style = pointStyle
+      //   }
+      //   style.color = assignColor(i)
+      //   const point = self._shapes[i] as Point2D
+      //   point.draw(context, ratio, style)
+      // }
+    }
   }
 
   public resize (_start: Vector2D, _end: Vector2D, _limit: Size2D): void {}
@@ -152,7 +152,7 @@ export class Polygon2D extends Label2D {
   //   return (this._shapes[0] as Polygon2D).toPoly()
   // }
 
-  public _updateShapes (_shapes: ShapeType[]): void {
+  public updateShapes (_shapes: ShapeType[]): void {
     // if (this._label !== null) {
     //   const poly = shapes[0] as PolyType
     //   if (!_.isEqual(this.toPoly, poly)) {
@@ -161,7 +161,7 @@ export class Polygon2D extends Label2D {
     // }
   }
 
-  private _updateShapesValues (_poly: PolygonType): void {
+  private updateShapesValues (_poly: PolygonType): void {
   //   const [poly2d, tl, tm, tr, rm, br, bm, bl, lm] = this._shapes
   //   const x1 = poly.x1
   //   const y1 = poly.y1
