b81a3271a36b21802a86a4bf72b0b8a9d629d282
Chris Powers <chris.powers@berkeley.edu>
2019-09-05 14:08:31 -0700

    finish testing all backend actions individually

--- server/http/actions_test.go
+++ server/http/actions_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
   "math/rand"
 	"path"
+  "reflect"
 	"testing"
 )
 
@@ -17,7 +18,7 @@ func checkRectsEqual(rect1 ShapeRect, rect2 ShapeRect) error {
       rect1.Y1 == rect2.Y1 && rect1.Y2 == rect2.Y2 {
         return nil
     }
-    return fmt.Errorf("Shape is incorrect")
+    return errors.New("Shape is incorrect")
 }
 
 // Check if an item's shape and label data has changed
@@ -25,12 +26,13 @@ func checkShapesAdded(item ItemData, numShapes int) error {
   if len(item.Labels) == numShapes && len(item.Shapes) == numShapes {
     return nil
   }
-  return fmt.Errorf("Number of shapes is incorrect")
+  return errors.New("Number of shapes is incorrect")
 }
 
 // Check if a shape was added correctly (to a new label)
 func checkShapeCorrect(item ItemData, numShapes int, labelId int,
-  shapeId int, addedShape ShapeRect) error {
+  addedShape ShapeRect) error {
+  shapeId := labelId
   shape := item.Shapes[shapeId].Shape
   shapesForLabel := item.Labels[labelId].Shapes
   err := checkShapesAdded(item, numShapes)
@@ -38,7 +40,7 @@ func checkShapeCorrect(item ItemData, numShapes int, labelId int,
     return err
   }
   if len(shapesForLabel) != 1 || shapesForLabel[0] != shapeId  {
-    return fmt.Errorf("Label is incorrect")
+    return errors.New("Label is incorrect")
   }
   return checkRectsEqual(addedShape, shape)
 }
@@ -110,11 +112,12 @@ func randomKey(dict map[int]LabelData) int {
 
 // Object used to store metadata about actions so they can be checked later
 type CheckData struct {
-   Shape     ShapeRect
-   LabelId   int
-   NumShapes int
-   ItemIndex int
-   ShapeId   int
+   Shape      ShapeRect
+   LabelId    int
+   NumShapes  int
+   ItemIndex  int
+   ShapeId    int
+   LabelProps LabelData
 }
 
 // Create a new AddLabel action, and use it to update the state
@@ -144,7 +147,7 @@ func checkAddLabel(state *TaskData, checkData CheckData) error {
   shape := checkData.Shape
   itemIndex := checkData.ItemIndex
   item := state.Items[itemIndex]
-  err := checkShapeCorrect(item, numShapes, labelId, labelId, shape)
+  err := checkShapeCorrect(item, numShapes, labelId, shape)
   if err != nil {
     return fmt.Errorf("Label not added correctly: %v", err)
   }
@@ -177,6 +180,39 @@ func checkChangeShape(state *TaskData, checkData CheckData) error {
   return nil
 }
 
+//Create a new ChangeLabel action, and use it to udpate the stae
+func runChangeLabel(state *TaskData, itemIndex int, labelId int) (
+  *TaskData, LabelData, error) {
+    props := LabelData{
+      Category: []int{1},
+      Attributes: map[string][]int{"a": []int{1}},
+      Parent: 0,
+      Children: []int{1, 2},
+    }
+    changeLabelAction := ChangeLabelAction{
+      ItemIndex: itemIndex,
+      LabelId: labelId,
+      Props: props,
+    }
+    newState, err := changeLabelAction.updateState(state)
+    return newState, props, err
+}
+
+//Check that a ChangeLabel action was executed correctly
+func checkChangeLabel(state *TaskData, checkData CheckData) error {
+  itemIndex := checkData.ItemIndex
+  labelId := checkData.LabelId
+  props := checkData.LabelProps
+  label := state.Items[itemIndex].Labels[labelId]
+  if !reflect.DeepEqual(props.Category, label.Category) ||
+    !reflect.DeepEqual(props.Attributes, label.Attributes) ||
+    props.Parent != label.Parent ||
+    !reflect.DeepEqual(props.Children, label.Children) {
+    return errors.New("Label props were not modified correctly")
+  }
+  return nil
+}
+
 //Create a new DeleteLabel action, and use it to update the state
 func runDeleteLabel(state *TaskData, itemIndex int, labelId int) (
   *TaskData, error) {
@@ -205,6 +241,8 @@ func checkAction(actionType string, state *TaskData, checkData CheckData) error 
     return checkAddLabel(state, checkData)
   case changeShape:
     return checkChangeShape(state, checkData)
+  case changeLabel:
+    return checkChangeLabel(state, checkData)
   case deleteLabel:
     return checkDeleteLabel(state, checkData)
   default:
@@ -267,6 +305,23 @@ func runActions(initialState *TaskData, actionQueue []string, maxItem int) error
         ShapeId: shapeId,
         ItemIndex: itemIndexWithShape,
       }
+    case changeLabel:
+      if itemIndexWithShape == -1 {
+        return errors.New("There are no labels to modify")
+      }
+      labelMap := states[i].Items[itemIndexWithShape].Labels
+      labelId := randomKey(labelMap)
+      returnState, props, err := runChangeLabel(
+        states[i], itemIndexWithShape, labelId)
+      if err != nil {
+        return fmt.Errorf("changeLabel failed: %v", err)
+      }
+      newState = returnState
+      newCheckData = CheckData{
+        LabelProps: props,
+        LabelId: labelId,
+        ItemIndex: itemIndexWithShape,
+      }
     case deleteLabel:
       if itemIndexWithShape == -1 {
         return errors.New("There are no labels to delete")
@@ -313,57 +368,76 @@ func runActions(initialState *TaskData, actionQueue []string, maxItem int) error
   return nil
 }
 
+// Runs the test corresponding to the action queue
+func runTest(t *testing.T, actionQueue []string, maxTask int) {
+  // Prepare initial state
+  initialState, err := readTaskData()
+  if err != nil {
+    t.Fatal(err)
+  }
+  err = runActions(initialState, actionQueue, maxTask)
+  if err != nil {
+    t.Fatal(err)
+  }
+}
+
+// Runs a test that is expected to fail
+func runFailTest(t *testing.T, actionQueue []string, maxTask int) {
+  // Prepare initial state
+  initialState, err := readTaskData()
+  if err != nil {
+    t.Fatal(err)
+  }
+  err = runActions(initialState, actionQueue, maxTask)
+  if err == nil {
+    t.Fatal("Test was expected to fail but did not")
+  }
+}
+
 // Test action updates for task data
 // Tests that the addLabel action works
 func TestAddLabel(t *testing.T) {
-  // Prepare initial state
-	initialState, err := readTaskData()
-  if err != nil {
-    t.Fatal(err)
-  }
-  // add some labels to one items
   actionQueue := []string{addLabel, addLabel, addLabel}
-  err = runActions(initialState, actionQueue, 0)
-  if err != nil {
-    t.Fatal(err)
-  }
-
+  // add some labels to one items
+  runTest(t, actionQueue, 0)
   // add some labels to different items
-  actionQueue = []string{addLabel, addLabel, addLabel}
-  err = runActions(initialState, actionQueue, 1)
-  if err != nil {
-    t.Fatal(err)
-  }
+  runTest(t, actionQueue, 1)
 }
 
 // Tests that the changeShape action works
 func TestChangeShape(t *testing.T) {
-  // Prepare initial state
-	initialState, err := readTaskData()
-  if err != nil {
-    t.Fatal(err)
-  }
-
   // add a label then change it a few times
   actionQueue := []string{addLabel, changeShape, changeShape}
-  err = runActions(initialState, actionQueue, 0)
-  if err != nil {
-    t.Fatal(err)
-  }
+  runTest(t, actionQueue, 0)
+
+  // error if you try to change nonexistent shapes
+  actionQueue = []string{changeShape}
+  runFailTest(t, actionQueue, 0)
+}
+
+// Tests that the changeLabel action works
+func TestChangeLabel(t *testing.T) {
+  // add some labels then modify them
+  actionQueue := []string{addLabel, addLabel, changeLabel, changeLabel}
+  runTest(t, actionQueue, 0)
+
+  // error if you try to modify nonexistent labels
+  actionQueue = []string{changeLabel}
+  runFailTest(t, actionQueue, 0)
 }
 
 // Tests that the deleteLabel action works
 func TestDeleteLabel(t *testing.T) {
-  // Prepare initial state
-	initialState, err := readTaskData()
-  if err != nil {
-    t.Fatal(err)
-  }
-
   // add some labels then delete them
   actionQueue := []string{addLabel, deleteLabel, addLabel, deleteLabel}
-  err = runActions(initialState, actionQueue, 0)
-  if err != nil {
-    t.Fatal(err)
-  }
+  runTest(t, actionQueue, 0)
+
+  // error if you try to delete nonexistent labels
+  actionQueue = []string{addLabel, deleteLabel, deleteLabel}
+  runFailTest(t, actionQueue, 0)
 }
+
+// Tests some random sequeneces of actions
+// func TestRandomizedActions(t *testing.T) {
+//
+// }
